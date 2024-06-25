35a355a862c2457fa49311cd8273e81b00b904f8
brfgomes <bruno_f_gomes@outlook.com>
2024-06-25 08:52:07 -0300

    first commit

---
+++ consumer/poc-rabbitmq-consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+    
+    Console.WriteLine(" [*] Agurdando mensagens.");
+    
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        
+        Console.WriteLine($" [x] Recebido: {message}");
+    };
+    await channel.BasicConsumeAsync(queue: "hello",
+        autoAck: true,
+        consumer: consumer);
+        
+        Console.WriteLine("Aperte [enter] para sair");
+        Console.ReadLine();
---
+++ sendler/poc-rabbitmq-sendler/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+Console.WriteLine("Digite sua mensagem e aperte <ENTER>");
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == "")
+        break;
+
+    var body = Encoding.UTF8.GetBytes(message);
+    var properties = new BasicProperties();
+    
+    await channel.BasicPublishAsync(exchange: string.Empty,
+        routingKey: "hello",
+        basicProperties: properties,
+        body: body,
+        mandatory: false,
+        cancellationToken: CancellationToken.None);
+    
+    Console.WriteLine($" [x] Enviado {message}");
+}
